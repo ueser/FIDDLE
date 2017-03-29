4235f51f95e25f5968765a135a3e7cd989961223
Umut Eser <umuteser@gmail.com>
2017-03-28 20:04:57 -0400

    clean up

--- fiddle/get_regions_from_ucsc.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-mysql --user=genome --host=genome-mysql.cse.ucsc.edu -A -D  -e sacCer3 \
-'SELECT chrom,txStart,txStart,"TSS",".",strand FROM gene WHERE strand = "+";' \
-| tail -n +2 > ../data/regions/tss.bed
