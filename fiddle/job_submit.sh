58c6fcd7bb1bb275f05963c6f081568e0e1fa72f
ueser <umuteser@gmail.com>
2017-03-31 10:44:46 -0400

    config changes

--- fiddle/job_submit.sh
+++ fiddle/job_submit.sh
@@ -1,6 +1,6 @@
 module load dev/tensorflow/1.0-GPU
 
-to_submit='python main.py --runName CN_DS2TS_lr1e-4 --learningRate 1e-4 --configuration CN2TS.config.json --dataDir ../data/hdf5datasets/CN2TS_WT_500bp_tss'
+to_submit='python main.py --runName DNAseq_prediction_withAE --learningRate 1e-3'
 
-bsub -q short -R "rusage[ngpus=1]" -W 10:00 -J CN_DS2TS_gpu_1e4 -u umuteser@gmail.com -N $to_submit
+bsub -q short -R "rusage[ngpus=1]" -W 10:00 -J DNAseq_predAE -u umuteser@gmail.com -N $to_submit
 
