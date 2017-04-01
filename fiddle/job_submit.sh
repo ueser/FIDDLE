e4fd42134e25a52f9e03e7e1e7e27f2617a12b3b
ueser <umuteser@gmail.com>
2017-04-01 00:48:56 -0400

    sc

--- fiddle/job_submit.sh
+++ fiddle/job_submit.sh
@@ -1,6 +1,6 @@
 module load dev/tensorflow/1.0-GPU
 
-to_submit='python main.py --runName DNAseq_prediction_withAE --learningRate 1e-3'
+to_submit='python main.py --runName cnds_ts_singlestranded --learningRate 1e-4'
 
-bsub -q short -R "rusage[ngpus=1]" -W 10:00 -J DNAseq_predAE -u umuteser@gmail.com -N $to_submit
+bsub -q short -R "rusage[ngpus=1]" -W 10:00 -J CNDS_TS_single -u umuteser@gmail.com -N $to_submit
 
